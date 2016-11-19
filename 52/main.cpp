60e87a1e70d431f72102be4baf734b2cf4f1d825
smeagolthellama <magardner2010@gmail.com>
2016-11-19 20:21:10 +0200

    added files.

--- 52/main.cpp
+++ 52/main.cpp
@@ -5,30 +5,20 @@ using namespace std;
 
 /**
  *
-51.		 Az s karakterlánc legfeljebb 52 kisbetűt (az angol ABC-ből) és számjegyeket tartalmaz. Az első  karakter  kisbetű,  az  utolsó  karakter  számjegy  és  minden  kisbetűt  egy  nem  nulla számjegy  követ.  Írj  egy  C/C++ programot,   amely  a  billentyűzetről  beolvassa   az  s karakterláncot, majd felépít és a képernyőre ír egy új, csak az angol abc kisbetűit tartalmazó karakterláncot,  amelyet  a  következő  szabály  szerint  állít  elő:  minden  kisbetű  annyiszor szerepel az új karakterláncban, amennyit az őt közvetlenül követő számjegy jelöl.
-Példa:  ha  a  billentyűzetről  beolvasott  karakterlánc:  a2b1f2,  akkor  az  új  karakterlánc: aabff. 	(10p)
+52.	 Írj egy C/C++ programot, amely beolvas a billentyűzetről egy zérótól különböző n (n≤100) természetes  számot,  majd  n darab  karakterláncot  (minden  karakterlánc  legfeljebb  30 karaktert tartalmaz,  amelyek az angol ABC kisbetűi),  majd  a képernyőre  írja, hogy az n karakterláncból hány áll csak magánhangzóból. Magánhangzónak tekintjük az a, e, i, o, u betűket.
+Példa:  ha  n=3,  és  a  mellékelt  karakterláncokat  olvassuk  be,           arc ae creion
+akkor a program által kiírt érték az 1, mert az ae karakterlánc csak magánhangzókból áll. 	(10p.)
+
  */
 
 int main()
 {
-	char t[CHAR_STAR_SIZE];
-	cin.getline(t,CHAR_STAR_SIZE);
-	uint_fast8_t i;
-	int a;
-	char tmp[2]="\0";
-	for(i=0;i<strlen(t);){
-		if (!(t[i]>'0'&&t[i]<'9')){
-			tmp[0]=t[i];
-			i++;
-			a=t[i]-'0';
-			for(int j=1;j<a;j++,i++){
-				beszur(t,tmp,i-1);
-			}
-			torles(t,i,1);
-		}else{
-			i++;
-		}
-	}
-	cout<<t<<endl;
+	int n,i;
+    cin>>n;
+    char c[n][30];
+    for(i=0;i<n;i++){
+        cin>>c[i];
+    }
+
     return 0;
 }
